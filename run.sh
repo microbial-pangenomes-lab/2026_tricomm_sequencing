b58fc98787952479f149f799951faccb567dfe41
Marco Galardini <marco.galardini@twincore.de>
2026-05-28 10:48:24 +0200

    New PN23 dataset

--- run.sh
+++ run.sh
@@ -12,6 +12,7 @@ mkdir -p out
 mkdir -p out/ancestrals
 mkdir -p out/pOXA48_rep1/
 mkdir -p out/pOXA48_rep2/
+mkdir -p out/PN23_rep1/
 
 # run breseq, using 12 cores concurrently
 parallel --jobs 12 --progress < jobs.sh
